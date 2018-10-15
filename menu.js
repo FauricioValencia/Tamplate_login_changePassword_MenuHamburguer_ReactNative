73a006906f90662dac93595d3435118e41486596
Wihermes <wihermes@MacBook-Pro-de-Wihermes.local>
2018-10-14 23:04:25 -0500

    first commit

---
+++ menu.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+class Menu extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Soy el menu perras</Text>
+            </View>
+        );
+    }
+}
+
+export default Menu;
